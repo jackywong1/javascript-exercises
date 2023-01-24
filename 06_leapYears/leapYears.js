eb1df94cfae1306c858ac0dfb9209e2b3e514201
Jacky Wong <z5115789@student.unsw.edu.au>
2023-01-24 23:53:24 +1100

    javascript exercises 01-06 completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(yearToBeTested) {
+    let isLeapYear = false;
+    if (yearToBeTested % 100 === 0 && yearToBeTested % 400 === 0) {
+        isLeapYear = true;
+    } else if (yearToBeTested % 100 !== 0 && yearToBeTested % 4 === 0) {
+        isLeapYear = true;
+    } else {
+        //do nothing;
+    }
+    return isLeapYear;
 };
-
 // Do not edit below this line
 module.exports = leapYears;
