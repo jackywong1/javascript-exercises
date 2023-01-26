ea1f32a6ff4bc4c85b93b008a6dea531a70ad179
Jacky Wong <z5115789@student.unsw.edu.au>
2023-01-26 19:49:21 +1100

    getTheTitles.js returns name object that contains the maximum age

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,8 +1,13 @@
 const getTheTitles = function(bookArr) {
-    let bookTitleArr = [];
-    for (let i = 0; i < bookArr.length; i++) {
-        bookTitleArr.push(bookArr[i].title);
-    }
+    // let bookTitleArr = [];
+    // for (let i = 0; i < bookArr.length; i++) {
+    //     bookTitleArr.push(bookArr[i].title);
+    // }
+    // return bookTitleArr;
+    // More efficient / cleaner approach
+    // array.map takes each element in the array and you 
+    // alter the element however you want and returns it as an array
+    let bookTitleArr = bookArr.map(book => book.title);
     return bookTitleArr;
 };
 
